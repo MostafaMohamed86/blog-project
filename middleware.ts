29f22ff0e4d971ed26c513490dfb9cf1cd1cc3e5
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2026-01-26 12:43:23 +0200

    removed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  // البحث عن الكوكي بالاسم الدقيق الذي وجدناه في متصفحك
-  const sessionToken =
-    request.cookies.get("__Secure-better-auth.session_token") ||
-    request.cookies.get("better-auth.session_token");
-
-  // التحقق من وجود الكوكي وقيمته
-  if (!sessionToken || !sessionToken.value) {
-    // إذا لم يجد الجلسة، يحول لصفحة تسجيل الدخول
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+export function middleware() {
+	return NextResponse.next();
   }
-
-  // إذا وجد الجلسة، يسمح بالمرور
-  return NextResponse.next();
-}
-
-export const config = {
-  // حماية المسارات المطلوبة وكل ما بداخلها
-  matcher: ["/blog/:path*", "/create/:path*"],
-};
