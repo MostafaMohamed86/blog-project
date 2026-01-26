8e8e2d8c9700d9813d1e9d8464e2f0b197f7e659
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2026-01-26 14:00:20 +0200

    removed dynamic

--- app/(shared-layout)/create/page.tsx
+++ app/(shared-layout)/create/page.tsx
@@ -2,8 +2,6 @@ import { isAuthenticated } from "@/lib/auth-server";
 import { redirect } from "next/navigation";
 import CreateForm from "./CreateForm";
 
-export const dynamic = "force-dynamic";
-
 export default async function CreatePage() {
   const auth = await isAuthenticated();
 
